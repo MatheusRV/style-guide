9240581b63da406586251cde8800bf061386f29a
Kxeta <caixeta.phillipe@gmail.com>
2015-08-19 14:47:08 -0300

    Menu e submenus para navegação

--- versao_antiga/js/rock_script.js
+++ versao_antiga/js/rock_script.js
@@ -1,7 +1,51 @@
 $(document).ready(function(){
-    $('.list-group-item').click(function(event) {
-    	$('.active').removeClass('active');
-    	$(event.target).addClass('active');
+	var nav = $('nav'),
+		nav_height = nav.outerHeight(),
+		sections = $('section');
 
+    $('.list-group-item').click(function(event) {
+    	var id = $(event.target).attr('href'); 
+		$('html, body').animate({
+		scrollTop: $(id).offset().top - nav_height
+		}, 500, function(){
+			$('.list-group-item.active').removeClass('active');
+			$('.parent-list-group:not(.active)').find('.sublist-group').css("display", "none");
+    		$(event.target).parent('.list-group-item').addClass('active');
+    		// Sublista nivel 1
+		  	$(event.target).parent('.list-group-item').parent('.list-group').parent('.parent-list-group').addClass('active');
+		  	// Sublista nivel 2
+		  	$(event.target).parent('.list-group-item').parent('.list-group').parent('.parent-list-group').parent('.list-group').parent('.parent-list-group').addClass('active');
+		  	$('.parent-list-group.active').find('.sublist-group:first').css("display", "block");
+		});
+		return false;
     });
+	 
+	$(window).on('scroll', function () {
+		if($(window).scrollTop() + $(window).height() == $(document).height()) {
+			$('.list-group-item.active').removeClass('active');
+		  	$('a[href="#concluindo"]').parent('.list-group-item').addClass('active');
+		}
+		else{
+			var cur_pos = $(this).scrollTop();
+			sections.each(function() {
+				var top = $(this).offset().top - nav_height,
+				    bottom = top + $(this).outerHeight();
+
+				if (cur_pos >= top && cur_pos <= bottom) {
+					$('.list-group-item.active').removeClass('active');
+					$('.parent-list-group:not(.active)').find('.sublist-group').css("display", "none");
+				  	$('a[href="#'+$(this).attr('id')+'"]').parent('.list-group-item').addClass('active');
+				  	// Sublista nivel 1
+				  	$('a[href="#'+$(this).attr('id')+'"]').parent('.list-group-item').parent('.list-group').parent('.parent-list-group').addClass('active');
+				  	// Sublista nivel 2
+				  	$('a[href="#'+$(this).attr('id')+'"]').parent('.list-group-item').parent('.list-group').parent('.parent-list-group').parent('.list-group').parent('.parent-list-group').addClass('active');
+				  	$('.parent-list-group.active').find('.sublist-group:first').css("display", "block");
+				}
+			});
+		}
+	});
+
+	if($('.parent-list-group').hasClass('active')){
+		$('.parent-list-group.active').find('.sublist-group:first').css("display", "block");
+	}
 });
