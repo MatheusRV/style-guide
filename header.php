d5c8dd4afbe292fd3cedd42ef16f31b726e28a80
Werik Gonçalves <ueriqui@gmail.com>
2017-03-07 11:18:35 -0300

    Volta ao 2016

---
+++ header.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, shrink-to-fit=no, initial-scale=1">
+    <meta name="description" content="O Guia de Estilo da Rock Content.">
+    <meta name="author" content="Rock Content">
+
+    <title>Guia de Estilo | Rock Content</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/simple-sidebar.css" rel="stylesheet">
+    <link href="css/custom.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+    <!-- Fontes -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,700|Roboto:400,500,700" rel="stylesheet">
+
+    <!-- Monitoramento -->
+
+</head>
+
+<body>
+    <header class="bs-docs-nav navbar navbar-fixed-top" id="top">
+      <div class="container">
+        <div class="navbar-header">
+          <!--button aria-controls="bs-navbar" aria-expanded="false" class="collapsed navbar-toggle" data-target="#bs-navbar" data-toggle="collapse" type="button"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button-->
+          <a href="/" class="navbar-brand"><img src="imgs/rockcontent-logo.png" alt="Guia de Estilo Rock Content" /></a>
+        </div>
+        <nav class="collapse navbar-collapse" id="bs-navbar">
+          <!--ul class="nav navbar-nav">
+            <li> <a href="../getting-started/">Getting started</a> </li>
+          </ul-->
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="https://rockcontent.com">Home</a></li>
+            <li><a href="https://rockcontent.com/materiais-gratuitos">Materiais Gratuitos</a></li>
+            <li><a href="http://plataforma.rockcontent.com">Login</a></li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+
+    <div id="wrapper">
+
+        <!-- Sidebar -->
+        <div id="sidebar-wrapper">
+            <ul class="sidebar-nav">
+                <li class="sidebar-brand hidden">
+                    <a href="#">
+                        Guia de Estilo
+                    </a>
+                </li>
+                <li>
+                    COMEÇANDO
+                </li>
+                <li>
+                    <a href="/introducao.php">Introdução</a>
+                </li>
+                <li>
+                    <a href="/tom-e-voz.php">Tom e voz da Rock Content</a>
+                </li>
+                <li>
+                    NOSSOS CANAIS
+                </li>
+                <li>
+                    <a href="/blog-marketing-de-conteudo.php">O blog Marketing de Conteúdo</a>
+                </li>
+                <li>
+                    <a href="/blog-saia-do-lugar.php">O blog Saia Do Lugar</a>
+                </li>
+                <li>
+                    <a href="/blog-escreva-para-web.php">O blog Escreva Para Web</a>
+                </li>
+                <li>
+                    <a href="/midias-sociais.php">Nossos canais de Mídias Sociais</a>
+                </li>
+                <li>
+                    <a href="/outros-canais.php">Outros canais da Rock Content</a>
+                </li>
+                <li>
+                    OUTROS
+                </li>
+                <li>
+                    <a href="/ofertas.php">Nossas ofertas</a>
+                </li>
+                <li>
+                    <a href="/emails.php">Nossos emails</a>
+                </li>
+                <li>
+                    <a href="/landing-pages.php">Landing Pages</a>
+                </li>
+                <li>
+                    <a href="/conversando-com-os-leads.php">Conversando com os Leads</a>
+                </li>
+                <li>
+                    MAIS
+                </li>
+                <li>
+                    <a href="/elementos-web.php">Elementos Web</a>
+                </li>
+                <li>
+                    <a href="/manual-da-marca.php">Manual da Marca</a>
+                </li>
+                <li>
+                    <a href="/dicionario.php">Dicionário</a>
+                </li>
+                <li>
+                    FINALIZANDO
+                </li>
+                <li>
+                    <a href="/conclusao.php">Conclusão</a>
+                </li>
+                <li>
+                    &nbsp;
+                </li>
+            </ul>
+        </div>
+        <!-- /#sidebar-wrapper -->
+
+        <!-- Page Content -->
+        <div id="page-content-wrapper">
+            <div class="container-fluid">
+              <!--a href="#menu-toggle" class="btn btn-default" id="menu-toggle"><span class="glyphicon glyphicon-menu-hamburger" aria-hidden="true"></span></a-->
+              <a href="#menu-toggle" id="menu-toggle"><span class="glyphicon glyphicon-menu-hamburger" aria-hidden="true"></span> MENU</a>
+            </div>
